e0fc59109750956c6736259e7845fb9dae21b2f3
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2024-02-09 15:41:27 +0200

    App.jsx update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Profile } from './Profile/Profile';
 import { FriendList } from './friendList/FriendList';
 import userData from '../userData.json';
-// import friends from '../friends.json';
 
 export function App() {
   return (
